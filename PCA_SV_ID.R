61684d3966e1b7b74de8935b1f495bdc7af99f72
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2023-01-11 07:48:59 +0000

    Update PCA_SV_ID.R

--- PCA_SV_ID.R
+++ PCA_SV_ID.R
@@ -22,6 +22,55 @@ theme_set(theme_bw())
 
 # All populations pca -----------------------------------------------------
 
-pca_allpops = read.pcadapt('Charr_Poly_All_Fixed_coords_maf05_geno95.bed', 
+allpops = read.pcadapt('Charr_Poly_All_Fixed_coords_maf05_geno95.bed', 
                            type = 'bed')
+
+## Elbows at K=4
+# pca_allpops = pcadapt(allpops, 
+#                            K = 60, 
+#                            method="mahalanobis", 
+#                            min.maf = 0.01)
+# plot(pca_allpops, 
+#      option = 'screeplot')
+
+pca_allpops = pcadapt(allpops, 
+                      K = 10, 
+                      method = 'mahalanobis', 
+                      min.maf = 0.01)
+
+plot(pca_allpops, 
+     option = 'screeplot')
+
+summary(pca_allpops)
+
+pca_allpops$singular.values
+sum(pca_allpops$singular.values)
+
+(sqrt(0.51083013)/1.634673)*100
+## 43.72%
+(sqrt(0.23428876)/1.634673)*100
+## 29.61%
+(sqrt(0.19223258)/1.634673)*100
+## 26.82%
+
+pca_allpops_scores = as_tibble(pca_allpops$scores) %>%
+  rename(PC1 = 1,
+         PC2 = 2,
+         PC3 = 3) %>%
+  dplyr::select(PC1,
+                PC2,
+                PC3) %>% 
+  write_csv('Charr_PCA_allpops_scores.csv')
+
+identifiers = read_csv('ggtree_labels.csv') %>% 
+  rename(FID = Population)
+
+meta_data = read_delim('Charr_Poly_All_Fixed_coords_maf05_geno95_notbed.ped')
+
+meta_data = data %>% 
+  dplyr::select(FID, 
+                IndividualID) %>% 
+  left_join(., 
+            identifiers,
+            by = '#FamilyID')
 
