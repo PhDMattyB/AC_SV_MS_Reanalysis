3e87cf1c5b6b30b94146b38d8f191fcafd7086d2
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2023-01-14 18:15:48 +0000

    Create invclust_round2.R

---
+++ invclust_round2.R
@@ -0,0 +1,20 @@
+##############################
+## Invclust across each chr
+##
+## Matt Brachmann (PhDMattyB)
+##
+## 2023-01-14
+##
+##############################
+
+setwd('~/AC_SV_MS_Data/invclust')
+
+library(patchwork)
+library(invClust)
+library(tidyverse)
+library(snpStats)
+
+
+theme_set(theme_bw())
+
+
