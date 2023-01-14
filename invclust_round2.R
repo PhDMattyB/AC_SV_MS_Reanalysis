de5e3a4c023d46ba9d4be346f2dd450ecbeddda4
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2023-01-14 18:35:16 +0000

    Update invclust_round2.R

--- invclust_round2.R
+++ invclust_round2.R
@@ -17,4 +17,36 @@ library(snpStats)
 
 theme_set(theme_bw())
 
+## Not in function
+'%!in%' <- function(x,y)!('%in%'(x,y))
+
+
+# lg1 sv detect -----------------------------------------------------------
+
+
+lg1_inv = read.plink(bed = 'Charr_Poly_All_Fixed_1.bed', 
+                        bim = 'Charr_Poly_All_Fixed_1.bim', 
+                        fam = 'Charr_Poly_All_Fixed_1.fam')
+
+lg1_geno = lg1_inv$genotypes
+lg1_map = lg1_inv$map 
+## check
+identical(lg1_map[,2],
+          colnames(lg1_geno))
+
+## 100 snps might be a lot to find any inversions
+
+lg1_inv_detect = data.frame(chr = 1, 
+                              LBP = 6793717, 
+                              RBP = 84732430, 
+                              reg = 'lg1_inversion')
+
+lg1_inver1 = invClust(roi = lg1_inv_detect, 
+                             wh = 1, 
+                             geno = lg1_geno, 
+                             annot = lg1_map, 
+                             dim = 2)
+
+plot(lg1_inver1)
+
 
