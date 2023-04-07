6dd6c51704a729c0bf35c91e248e43842d0b7635
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2023-04-07 13:31:28 +0100

    Update Admixture.R

--- Admixture.R
+++ Admixture.R
@@ -8,3 +8,14 @@
 ##############################
 
 
+setwd('~/Bradbury_Postdoc/AC_SV_MS_Data/Admixture/')
+
+
+library(tidyverse)
+
+
+qvalues = read_table('Charr_Poly_All_Fixed_coords_maf05_geno95.4.Q', 
+                   col_names = c('Q1', 
+                                 'Q2', 
+                                 'Q3', 
+                                 'Q4'))
