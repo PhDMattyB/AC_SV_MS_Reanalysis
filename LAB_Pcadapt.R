b6955b4fba78768bfce0f697c2336d321bcb3564
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2023-04-13 18:28:23 +0100

    Update LAB_Pcadapt.R

--- LAB_Pcadapt.R
+++ LAB_Pcadapt.R
@@ -9,6 +9,7 @@ library(devtools)
 # install_github('isglobal-brge/invClust')
 library(invClust)
 library(patchwork)
+library(viridis)
 
 theme_set(theme_bw())
 
@@ -16,29 +17,21 @@ theme_set(theme_bw())
 
 # All populations pca -----------------------------------------------------
 
-allpops = read.pcadapt('Charr_Poly_All_Fixed_coords_maf05_geno95_envmatch_Lab.bed', 
+lab = read.pcadapt('Charr_Poly_All_Fixed_coords_maf05_geno95_envmatch_Lab.bed', 
                        type = 'bed')
 
-## Elbows at K=4
-# pca_allpops = pcadapt(allpops, 
-#                            K = 60, 
-#                            method="mahalanobis", 
-#                            min.maf = 0.01)
-# plot(pca_allpops, 
-#      option = 'screeplot')
-
-pca_allpops = pcadapt(allpops, 
+pca_lab = pcadapt(lab, 
                       K = 10, 
                       method = 'mahalanobis', 
                       min.maf = 0.01)
 
-plot(pca_allpops, 
+plot(pca_lab, 
      option = 'screeplot')
 
-summary(pca_allpops)
+summary(pca_lab)
 
-pca_allpops$singular.values
-sum(pca_allpops$singular.values)
+pca_lab$singular.values
+sum(pca_lab$singular.values)
 
 (sqrt(0.26759009)/1.141238)*100
 ## 45.32%
@@ -47,14 +40,14 @@ sum(pca_allpops$singular.values)
 (sqrt(0.11862102)/1.141238)*100
 ## 30.18%
 
-pca_allpops_scores = as_tibble(pca_allpops$scores) %>%
+pca_lab_scores = as_tibble(pca_lab$scores) %>%
   rename(PC1 = 1,
          PC2 = 2,
          PC3 = 3) %>%
   dplyr::select(PC1,
                 PC2,
                 PC3) %>% 
-  write_csv('LAB_Charr_PCA_allpops_scores.csv')
+  write_csv('LAB_Charr_pca_lab_scores.csv')
 
 identifiers = read_csv('ggtree_labels.csv') %>% 
   rename(FID = Population)
@@ -68,7 +61,7 @@ meta_data = read_table2('Lab_Charr.ped',
             identifiers, 
             by = 'FID') %>% 
   bind_cols(., 
-            pca_allpops_scores)
+            pca_lab_scores)
 
 
 # glacial_cols = c('#F23545',
@@ -105,13 +98,110 @@ meta_data  %>%
        y = 'Principal component 2 (32.1%)', 
        col = 'Glacial lineages')
 
-ggsave(file = 'PCAdapt_all_pops_k4_Glacial_lineages.tiff',
+ggsave(file = 'PCAdapt_Labrador.tiff',
        path = '~/Bradbury_Postdoc/AC_SV_MS_Data/Figures/', 
        plot = last_plot(), 
        dpi = 'retina', 
        units = 'cm', 
        width = 20.0, 
        height = 13)
+
+
+# pca glacial lineages ----------------------------------------------------
+
+
+allpops = read.pcadapt('Charr_Poly_All_Fixed_coords_maf05_geno95.bed', 
+                       type = 'bed')
+
+
+pca_allpops = pcadapt(allpops, 
+                      K = 10, 
+                      method = 'mahalanobis', 
+                      min.maf = 0.01)
+
+plot(pca_allpops, 
+     option = 'screeplot')
+
+summary(pca_allpops)
+
+pca_allpops$singular.values
+sum(pca_allpops$singular.values)
+
+(sqrt(0.51083013)/1.634673)*100
+## 43.72%
+(sqrt(0.23428876)/1.634673)*100
+## 29.61%
+(sqrt(0.19223258)/1.634673)*100
+## 26.82%
+
+pca_allpops_scores = as_tibble(pca_allpops$scores) %>%
+  rename(PC1 = 1,
+         PC2 = 2,
+         PC3 = 3) %>%
+  dplyr::select(PC1,
+                PC2,
+                PC3) %>% 
+  write_csv('Charr_PCA_allpops_scores.csv')
+
+identifiers = read_csv('ggtree_labels.csv') %>% 
+  rename(FID = Population)
+
+meta_data = read_table2('Charr_Poly_All_Fixed_coords_maf05_geno95_notbed.ped', 
+                        col_names = F) %>% 
+  dplyr::select(X1:X2) %>% 
+  rename(FID = X1, 
+         IID = X2) %>% 
+  left_join(., 
+            identifiers, 
+            by = 'FID') %>% 
+  bind_cols(., 
+            pca_allpops_scores)
+
+
+glacial_cols = c('#F23545',
+                          '#4E458C',
+                          '#4E94BF',
+                          '#F29F05')
+
+# meta_data %>% 
+#   group_by(FID)
+
+meta_data  %>% 
+  ggplot(aes(x = PC1, 
+             y = PC2))+
+  geom_point(aes(col = Glacial_lin),
+             size = 2)+
+  # geom_point(aes(col = Lat), 
+  #            size = 2)+
+  # geom_point(aes(col = Population), 
+  #            size = 2)+
+  # scale_color_manual(values = cols)+
+  scale_color_manual(values = glacial_cols)+
+  # scale_color_viridis(option = 'magma')+
+  # scale_fill_viridis(option = 'magma')+
+  theme(panel.grid.major = element_blank(),
+        panel.grid.minor = element_blank(), 
+        axis.title = element_text(size = 15), 
+        axis.text = element_text(size = 15), 
+        axis.ticks = element_line(size = 1), 
+        plot.title = element_text(size = 15, 
+                                  hjust = 0), 
+        legend.text = element_text(size = 12),
+        legend.title = element_text(size = 14), 
+        legend.position = 'none') +
+  labs(x = 'Principal component 1 (43.7%)',
+       y = 'Principal component 2 (29.6%)', 
+       col = 'Glacial lineages')
+
+ggsave(file = 'PCAdapt_Labrador.tiff',
+       path = '~/Bradbury_Postdoc/AC_SV_MS_Data/Figures/', 
+       plot = last_plot(), 
+       dpi = 'retina', 
+       units = 'cm', 
+       width = 20.0, 
+       height = 13)
+
+
 
 
 # detect sv per chr -------------------------------------------------------
