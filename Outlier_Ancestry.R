5c941cb54e0cdfb378ca1b404a67d3ecab00f57b
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2023-04-13 19:47:59 +0100

    Update Outlier_Ancestry.R

--- Outlier_Ancestry.R
+++ Outlier_Ancestry.R
@@ -9,13 +9,26 @@
 
 # Make --keep files for plink -------------------------------
 
-ped_test = read_table2('Charr_Poly_All_Fixed_coords_maf05_geno95_notbed.ped', 
-                       col_names = F)
+# ped_test = read_table2('Charr_Poly_All_Fixed_coords_maf05_geno95_notbed.ped', 
+#                        col_names = F)
 
 ped_ids = read_table2('Charr_Poly_All_Fixed_coords_maf05_geno95.fam', 
                       col_names = F) %>%
   dplyr::select(X1,
-                X2)
+                X2) %>% 
+  rename(Population = X1, 
+         Individual = X2)
+
+latlong = read_csv('~/Bradbury_Postdoc/AC_SV_MS_Data/Admixture/SampleSites_Coords_1June2020.csv')
+
+
+Data = inner_join(ped_ids, 
+                        latlong) %>% 
+  dplyr::select(Population, 
+                Individual, 
+                Glacial_lin, 
+                Lat, 
+                Long)
 
 ATL %>% 
   dplyr::select(X1, 
