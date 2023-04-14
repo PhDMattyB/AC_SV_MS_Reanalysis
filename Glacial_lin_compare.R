2cefaafee93db83bf14c6a23e8dabe5ff652370f
Matt Brachmann <35701547+PhDMattyB@users.noreply.github.com>
2023-04-14 15:12:52 +0100

    Create Glacial_lin_compare.R

---
+++ Glacial_lin_compare.R
@@ -0,0 +1,13 @@
+##############################
+##  Glacial lineage Fst
+##
+## Matt Brachmann (PhDMattyB)
+##
+## 14.04.2023
+##
+##############################
+
+setwd('~/Bradbury_Postdoc/AC_SV_MS_Data/Pcadapt/')
+
+library(tidyverse)
+
